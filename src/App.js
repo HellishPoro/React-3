4bc37dea0ca684607f9882b3e9c13555fc0baf55
Leonid <borl99@inbox.ru>
2024-08-03 01:45:44 +0300

    Готовый калькулятор

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import { useState } from 'react';
+
+const NUMS = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0']
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [operand1, setOperand1] = useState('0')
+	const [operand2, setOperand2] = useState('')
+	const [operator, setOperator] = useState('')
+	const [result, setResult] = useState(false)
+	let displayingValues = operand1 + operator + operand2
+
+	const clickButton = (item) => {
+		if (operand1 === '0') {
+			setOperand1(item)
+		} else if (operator) {
+			setOperand2((pastMeaning) => pastMeaning + item)
+		} else {
+			setOperand1((pastMeaning) => pastMeaning + item)
+		}
+	}
+
+	const clickOnTheSigns = (op) => {
+		switch (op) {
+			case '+':
+				setOperator('+')
+				break
+			case '-':
+				setOperator('-')
+				break
+			case '*':
+				setOperator('*')
+				break
+			case 'C':
+				setOperand1('0')
+				setOperand2('')
+				setOperator('')
+				setResult(false)
+				break
+			default:
+				break
+		}
+	}
+
+	const finalValue = () => {
+		switch (operator) {
+			case '+':
+				setOperand1(Number(operand1) + Number(operand2))
+				setOperand2('')
+				setOperator('')
+				setResult(true)
+				break
+			case '-':
+				setOperand1(Number(operand1) - Number(operand2))
+				setOperand2('')
+				setOperator('')
+				setResult(true)
+				break
+			case '*':
+				setOperand1(Number(operand1) * Number(operand2))
+				setOperand2('')
+				setOperator('')
+				setResult(true)
+				break
+			default:
+				break
+		}
+	}
+
+
+
+
+
+
+	return (
+		<div className={styles.calculator}>
+			<input className={`${styles.input} ${result ? styles.result : ''}`} value={displayingValues} />
+			<div className={styles.meanings}>{NUMS.map((item, index) =>
+				<button className={styles.button} onClick={() => clickButton(item)} key={index}>{item}</button>)}
+				<button className={styles.button} onClick={() => clickOnTheSigns('-')}>-</button>
+				<button className={styles.button} onClick={() => clickOnTheSigns('+')}>+</button>
+				<button className={styles.button} onClick={() => clickOnTheSigns('*')}>*</button>
+				<button className={styles.button} onClick={finalValue}>=</button>
+				<button className={styles.button} onClick={() => clickOnTheSigns('C')}>C</button>
+			</div>
+		</div>
+	)
+
 }
 
 export default App;
